12c7075004639b909ef71fb641605295c22f0bc0
Robert Eisele <robert@xarg.org>
2015-07-13 19:44:32 +0200

    Fixed missing abs

--- complex.js
+++ complex.js
@@ -485,7 +485,7 @@
 
         parse(a, b);
 
-        return (P['r'] - this['r']) <= EPSILON && (P['i'] - this['i'] <= EPSILON);
+        return Math.abs(P['r'] - this['r']) <= EPSILON && Math.abs(P['i'] - this['i'] <= EPSILON);
     };
 
     /**
