043e187f27f1e3188bcd3fb75991e403c9cb68d6
Harry Sarson <harry.sarson@hotmail.co.uk>
2017-09-03 14:25:15 +0100

    Splits the constructor and function tests into different mocha descriptions

--- tests/complex.test.js
+++ tests/complex.test.js
@@ -679,7 +679,7 @@ function describeTest(test) {
   return ctor + fnCall + expectedResult + error;
 }
 
-describe("Complex", function () {
+describe("Complex functions", function () {
 
   for (var i = 0; i < functionTests.length; i++) {
 
@@ -698,6 +698,9 @@ describe("Complex", function () {
       });
     })(functionTests[i]);
   }
+});
+
+describe("Complex constructor", function() {
 
   for (var i = 0; i < constructorTests.length; i++) {
 
